5a5027b2c04c166f033b805e88147078e73d2bb3
Samuel Mota <smagnun@gmail.com>
2020-08-21 22:29:12 +0200

    react-native app design

--- proffy-app/App.tsx
+++ proffy-app/App.tsx
@@ -1,27 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from 'expo';
+
+// one of them has to import 'useFonts'
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 // PAGES
-import Landing from './src/pages/Landing';
+// import Landing from './src/pages/Landing';
+
+// ROUTES
+import AppStack from "./src/routes/AppStack";
 
 
 export default function App() {
-  return (
-    <Landing />
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular, 
+    Archivo_700Bold,
+    Poppins_400Regular, 
+    Poppins_600SemiBold
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#333",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 32,
-    color: '#fff',
-    fontWeight: 'bold'
+  // while fonts are not loaded, shows the main page (AppLoading)
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      // fragment, it's like a div but it's not send to html <> </>
+      <> 
+        <AppStack />
+        <StatusBar style="inverted" />
+      </>
+    );
   }
-});
+}
---
+++ proffy-app/src/@types/index.d.ts
@@ -0,0 +1,4 @@
+// says to typescript that all files with *.png is a file that we can import in the application
+
+declare module '*.png';
+declare module '*.jpg';
